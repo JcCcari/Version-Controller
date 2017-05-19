e410857f1fed869ae5a94a98ede64b68ab96478f
JcCcari <jhoel.ccari@ucsp.edu.pe>
2017-05-19 09:46:11 -0500

    Estado: insert completo, find en proceso Compilable: Si

---
+++ cmake-build-debug/CMakeFiles/VersionController.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jhoelccari/CLionProjects/VersionController/graph.cpp" "/home/jhoelccari/CLionProjects/VersionController/cmake-build-debug/CMakeFiles/VersionController.dir/graph.cpp.o"
+  "/home/jhoelccari/CLionProjects/VersionController/main.cpp" "/home/jhoelccari/CLionProjects/VersionController/cmake-build-debug/CMakeFiles/VersionController.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/VersionController.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VersionController.dir/main.cpp.o"
+  "CMakeFiles/VersionController.dir/graph.cpp.o"
+  "VersionController.pdb"
+  "VersionController"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VersionController.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
